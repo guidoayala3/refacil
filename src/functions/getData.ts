d567facb7b071c837a2ef0a65964e8aae63643fa
guidoayala3 <guidoayala@hotmail.comgit config --global user.name guidoayalagit config --global user.email guidoayala3@gmail.com>
2023-08-30 15:46:06 -0500

    ajuste cambios

--- src/functions/getData.ts
+++ src/functions/getData.ts
@@ -2,15 +2,9 @@ import 'source-map-support/register';
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
 import middyfy from '@libs/lambda';
 import CasosType from '@src/structures/casos.type';
-import dbService from '../common/dbService';
-import { errorResponse, Response, successResponse, erroValidationDataResponse } from 'src/common/apiResponses';
+import { errorResponse, Response, successResponse  } from 'src/common/apiResponses';
 import CovidService from "../services/covidServices";
 
-async function getCasosCovid(): Promise<CasosType[]> {
-  const query = 'SELECT * FROM casos_covid';
-  const casos: CasosType[] = await dbService.query(query);
-  return casos;
-}
 
 
 const get: ValidatedEventAPIGatewayProxyEvent<CasosType> = async (
